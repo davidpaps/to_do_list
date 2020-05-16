16674d1c201b0a65a320ea5a7e144653ced0051d
David Papamichael <davidpapamichael@gmail.com>
2020-05-16 11:58:26 +0100

    postman initialized with project, synced with pg database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 app.use(cors());
 app.use(express.json());
+
+// create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+  } catch (error) {
+    console.error(err.message);
+  }
+});
+
+//  get all todo's
+//  get a todo
+// update a todo
+//  delete a todo
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
