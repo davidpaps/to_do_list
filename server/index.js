9110407851dfc5f48b1b1f1ef214c870ed126bf8
David Papamichael <davidpapamichael@gmail.com>
2020-05-16 12:30:33 +0100

    get a todo route completed

--- server/index.js
+++ server/index.js
@@ -16,14 +16,34 @@ app.post("/todos", async (req, res) => {
     );
 
     res.json(newTodo.rows[0]);
-  } catch (error) {
+  } catch (err) {
     console.error(err.message);
   }
 });
 
 //  get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-//  get a todo
+// get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 // update a todo
 //  delete a todo
 
