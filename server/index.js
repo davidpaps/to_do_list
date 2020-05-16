27c6eaa93f38b8ad9fa6e591f3dce05f9814141f
David Papamichael <davidpapamichael@gmail.com>
2020-05-16 12:46:23 +0100

    RESTful API of PG complete with delete path

--- server/index.js
+++ server/index.js
@@ -61,7 +61,20 @@ app.put("/todos/:id", async (req, res) => {
     console.error(err.message);
   }
 });
+
 //  delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json("Todo was deleted!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
