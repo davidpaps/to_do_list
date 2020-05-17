d03c428ffc1fad0e0478af00b35d9410175351fc
David Papamichael <davidpapamichael@gmail.com>
2020-05-17 13:36:09 +0100

    input form populates the database

--- client/src/components/inputTodo.js
+++ client/src/components/inputTodo.js
@@ -3,10 +3,25 @@ import React, { Fragment, useState } from "react";
 const InputTodo = () => {
   const [description, setDescription] = useState("");
 
+  const onSubmitForm = async (event) => {
+    event.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <Fragment>
       <h1 className="text-center mt-5">Input Todo</h1>
-      <form className="d-flex mt-5">
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
           className="form-control"
