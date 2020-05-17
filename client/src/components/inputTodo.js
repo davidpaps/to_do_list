4ed63f5896f9315e3223afcf7335f613910a4872
David Papamichael <davidpapamichael@gmail.com>
2020-05-17 13:05:45 +0100

    react hook setstte used to gain value from inout field

--- client/src/components/inputTodo.js
+++ client/src/components/inputTodo.js
@@ -1,12 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
+  const [description, setDescription] = useState("");
+
   return (
     <Fragment>
       <h1 className="text-center mt-5">Input Todo</h1>
-      <form>
-        <input type="text" className="" />
-        <button>Add Todo</button>
+      <form className="d-flex mt-5">
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+        <button className="btn btn-success">Add</button>
       </form>
     </Fragment>
   );
